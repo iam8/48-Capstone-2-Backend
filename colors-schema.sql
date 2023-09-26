1e75e092734e71461dcb33a5ea4b98f9a043f029
alexm <alex-m@outlook.com>
2023-09-26 13:01:01 -0700

    Add schema for 2 more tables

--- colors-schema.sql
+++ colors-schema.sql
@@ -5,3 +5,17 @@ CREATE TABLE users (
     last_name TEXT NOT NULL,
     is_admin BOOLEAN NOT NULL DEFAULT FALSE
 );
+
+CREATE TABLE collections (
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    creator_username VARCHAR(25) NOT NULL
+        REFERENCES users ON DELETE CASCADE
+);
+
+CREATE TABLE collections_colors (
+    collection_id INTEGER
+        REFERENCES collections ON DELETE CASCADE,
+    color_hex VARCHAR(6) NOT NULL
+    PRIMARY KEY (collection_id, color_hex)
+);
