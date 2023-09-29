585cc99ee0bafa5339799be7792718d203a9462b
alexm <alex-m@outlook.com>
2023-09-29 12:15:47 -0700

    Add route files; add code for auth routes (without JSON schema validation)

---
+++ routes/auth.js
@@ -0,0 +1,59 @@
+"use strict";
+
+/** Routes for authentication. */
+
+const express = require("express");
+const router = new express.Router();
+
+const User = require("../models/user");
+// const { BadRequestError } = require("../expressError");
+const { createToken } = require("../helpers/tokens");
+
+
+/** POST /auth/token: { username, password } => { token }
+ *
+ * Returns JWT token which can be used to authenticate further requests.
+ *
+ * Authorization required: none
+ */
+router.post("/token", async function (req, res, next) {
+    try {
+        // const validator = jsonschema.validate(req.body, userAuthSchema);
+        // if (!validator.valid) {
+        //     const errs = validator.errors.map(e => e.stack);
+        //     throw new BadRequestError(errs);
+        // }
+
+        const { username, password } = req.body;
+        const user = await User.authenticate(username, password);
+        const token = createToken(user);
+        return res.json({ token });
+    } catch (err) {
+        return next(err);
+    }
+});
+
+
+/** POST /auth/register: { user } => { token }
+ *
+ * User data must include: { username, password, firstName, lastName, email }
+ *
+ * Returns JWT token which can be used to authenticate further requests.
+ *
+ * Authorization required: none
+ */
+router.post("/register", async function (req, res, next) {
+    try {
+        // const validator = jsonschema.validate(req.body, userRegisterSchema);
+        // if (!validator.valid) {
+        //     const errs = validator.errors.map(e => e.stack);
+        //     throw new BadRequestError(errs);
+        // }
+
+        const newUser = await User.register({ ...req.body, isAdmin: false });
+        const token = createToken(newUser);
+        return res.status(201).json({ token });
+    } catch (err) {
+        return next(err);
+    }
+});
