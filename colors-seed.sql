b26d45ac7d9eea682e89e704d2a055445522c2b4
alexm <alex-m@outlook.com>
2023-09-28 18:41:16 -0700

    Add collections and colors to database

--- colors-seed.sql
+++ colors-seed.sql
@@ -22,3 +22,57 @@ VALUES
         'last2',
         FALSE
     );
+
+
+-- Add some collections to database
+INSERT INTO collections
+    (
+        title,
+        creator_username
+    )
+VALUES
+    (
+        'collection1-1',
+        'example1'
+    ),
+        (
+        'collection1-2',
+        'example1'
+    ),
+        (
+        'collection1-3',
+        'example1'
+    ),
+    (
+        'collection2-1',
+        'example2'
+    );
+
+
+-- Add some colors to collections
+INSERT INTO collections_colors
+    (
+        collection_id,
+        color_hex
+    )
+VALUES
+    (
+        '1',
+        '000000'
+    ),
+    (
+        '1',
+        '111111'
+    ),
+    (
+        '1',
+        '111112'
+    ),
+    (
+        '2',
+        'ffffff'
+    ),
+    (
+        '3',
+        'ffffff'
+    );
