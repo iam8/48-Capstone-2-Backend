afb62c88f56cbf3dcdf97c96d7025012e0209db6
alexm <alex-m@outlook.com>
2023-11-06 19:01:24 -0800

    Import jsonschema and schemas; add schema validation in routes

--- routes/users.js
+++ routes/users.js
@@ -6,10 +6,14 @@
 */
 
 const express = require("express");
+const jsonschema = require("jsonschema");
 
 const { ensureCorrectUserOrAdmin, ensureAdmin } = require("../middleware/auth");
 const User = require("../models/user");
 const { createToken } = require("../helpers/tokens");
+
+const userNewSchema = require("../schemas/userNew.json");
+const userUpdateSchema = require("../schemas/userUpdate.json");
 
 const router = express.Router();
 
@@ -29,11 +33,11 @@ const router = express.Router();
  **/
 router.post("/", ensureAdmin, async function (req, res, next) {
     try {
-        // const validator = jsonschema.validate(req.body, userNewSchema);
-        // if (!validator.valid) {
-        //     const errs = validator.errors.map(e => e.stack);
-        //     throw new BadRequestError(errs);
-        // }
+        const validator = jsonschema.validate(req.body, userNewSchema);
+        if (!validator.valid) {
+            const errs = validator.errors.map(e => e.stack);
+            throw new BadRequestError(errs);
+        }
 
         const user = await User.register(req.body);
         const token = createToken(user);
@@ -89,11 +93,11 @@ router.get("/:username", ensureCorrectUserOrAdmin, async function (req, res, nex
  **/
 router.patch("/:username", ensureCorrectUserOrAdmin, async function (req, res, next) {
     try {
-        // const validator = jsonschema.validate(req.body, userUpdateSchema);
-        // if (!validator.valid) {
-        //     const errs = validator.errors.map(e => e.stack);
-        //     throw new BadRequestError(errs);
-        // }
+        const validator = jsonschema.validate(req.body, userUpdateSchema);
+        if (!validator.valid) {
+            const errs = validator.errors.map(e => e.stack);
+            throw new BadRequestError(errs);
+        }
 
         const user = await User.update(req.params.username, req.body);
         return res.json({ user });
