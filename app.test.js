a2f3e89b3666673837e781ba454a6c607939143d
alexm <alex-m@outlook.com>
2023-11-09 16:43:25 -0800

    Add tests for app file

---
+++ app.test.js
@@ -0,0 +1,23 @@
+const request = require("supertest");
+
+const app = require("./app");
+const db = require("./colors-db");
+
+
+test("Not found for site 404", async () => {
+    const resp = await request(app).get("/no-such-path");
+    expect(resp.status).toEqual(404);
+});
+
+
+test("Not found for site 404 (test stack print)", async () => {
+    process.env.NODE_ENV = "";
+    const resp = await request(app).get("/no-such-path");
+    expect(resp.status).toEqual(404);
+    delete process.env.NODE_ENV;
+});
+
+
+afterAll(() => {
+    db.end();
+});
