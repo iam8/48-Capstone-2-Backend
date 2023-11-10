a08f19b563deecf6287857931cc4b14cf64810e9
alexm <alex-m@outlook.com>
2023-11-10 12:28:13 -0800

    Remove test comments

--- server.js
+++ server.js
@@ -6,6 +6,3 @@ const { HOSTNAME, PORT } = require("./config");
 app.listen(PORT, HOSTNAME, () => {
     console.log(`Started on http://${HOSTNAME}:${PORT}`);
 });
-
-// Here is a change made on the branch named 'tests'
-// Another comment to test upstream branch creation
