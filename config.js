5dd7b5fd545528a3b2d077fe8a0b959fb1c851b5
alexm <alex-m@outlook.com>
2023-11-10 13:33:48 -0800

    Retrieve HOSTNAME from env variables if defined; display HOSTNAME in console

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 require("colors");
 
 const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
-const HOSTNAME = "127.0.0.1";
+const HOSTNAME = process.env.HOSTNAME || "127.0.0.1";
 const PORT = +process.env.PORT || 3001;
 
 // Use dev database, testing database, or production database
@@ -20,6 +20,7 @@ const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
 
 console.log("Colors Config:".green);
 console.log("SECRET_KEY:".yellow, SECRET_KEY);
+console.log("HOSTNAME:".yellow, HOSTNAME);
 console.log("PORT:".yellow, PORT.toString());
 console.log("BCRYPT_WORK_FACTOR".yellow, BCRYPT_WORK_FACTOR);
 console.log("Database:".yellow, getDatabaseUri());
