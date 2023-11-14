0b0a9f6051f96f3c77a768d2318096c966830692
alexm <alex-m@outlook.com>
2023-11-14 14:41:15 -0800

    Add 2 tests for User.register()

--- models/user.test.js
+++ models/user.test.js
@@ -63,6 +63,24 @@ describe("authenticate()", () => {
 // Tests for register() ---------------------------------------------------------------------------
 describe("Tests for register()", () => {
     test("Returns correct user data and adds correct user data to database", async () => {
+        const newUser = {
+            username: "newUser",
+            firstName: "FIRST",
+            lastName: "LAST",
+            isAdmin: false
+        };
+
+        const regResult = await User.register({...newUser, password: "newPassword"});
+        expect(regResult).toEqual(newUser);
+
+        const dbResult = await db.query(`SELECT * FROM users WHERE username = 'newUser'`);
+        const dbUser = dbResult.rows[0];
+        expect(dbResult.rows).toHaveLength(1);
+        expect(dbUser.password.startsWith("$2b$")).toBe(true);
+        expect(dbUser.is_admin).toBe(false);
+    })
+
+    test("Returns correct admin data and adds correct admin data to database", async () => {
         const newAdmin = {
             username: "newAdmin",
             firstName: "ADMINFIRST",
@@ -80,13 +98,29 @@ describe("Tests for register()", () => {
         expect(dbUser.is_admin).toBe(true);
     })
 
-    // test("New user defaults to non-admin", async () => {
+    test("Throws BadRequestError on duplicate username", async () => {
+        expect.assertions(2);
 
-    // })
+        const dupUser = {
+            username: usernames[0],
+            firstName: "FIRST",
+            lastName: "LAST",
+            isAdmin: false
+        };
 
-    // test("Throws BadRequestError on duplicate username", async () => {
+        try {
+            await User.register({...dupUser, password: "newPassword"});
+        } catch(err) {
+            expect(err).toBeInstanceOf(BadRequestError);
+        }
 
-    // })
+        const dbResult = await db.query(`
+            SELECT * FROM users WHERE username = $1`,
+            [usernames[0]]
+        );
+
+        expect(dbResult.rows).toHaveLength(1);
+    })
 })
 
 //-------------------------------------------------------------------------------------------------
